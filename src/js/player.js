a019293f2027d5304ed9fd1ae65d811eebf147b1
Tao Guo <guotao945@gmail.com>
2017-11-30 17:18:50 +0800

    fix controls not be hidden when use keyboard

--- src/js/player.js
+++ src/js/player.js
@@ -605,7 +605,7 @@ class MediaElementPlayer {
 
 		doAnimation = doAnimation === undefined || doAnimation;
 
-		if (forceHide !== true && (!t.controlsAreVisible || t.options.alwaysShowControls || t.keyboardAction ||
+		if (forceHide !== true && (!t.controlsAreVisible || t.options.alwaysShowControls ||
 			(t.media.paused && t.media.readyState === 4 && ((!t.options.hideVideoControlsOnLoad &&
 			t.media.currentTime <= 0) || (!t.options.hideVideoControlsOnPause && t.media.currentTime > 0))) ||
 			(t.isVideo && !t.options.hideVideoControlsOnLoad && !t.media.readyState) ||
@@ -979,7 +979,9 @@ class MediaElementPlayer {
 			t.container.addEventListener('focusin', function (e) {
 				dom.removeClass(e.currentTarget, `${t.options.classPrefix}container-keyboard-inactive`);
 				if (t.controlsEnabled && !t.options.alwaysShowControls) {
-					t.showControls(false);
+					t.killControlsTimer('enter');
+					t.showControls();
+					t.startControlsTimer(t.options.controlsTimeoutMouseEnter);
 				}
 			});
 
@@ -989,8 +991,8 @@ class MediaElementPlayer {
 					if (e.relatedTarget) {
 						if (t.keyboardAction && !e.relatedTarget.closest(`.${t.options.classPrefix}container`)) {
 							t.keyboardAction = false;
-							if (t.isVideo && !t.options.alwaysShowControls) {
-								t.hideControls(true);
+							if (t.isVideo && !t.options.alwaysShowControls && !t.media.paused) {
+								t.startControlsTimer(t.options.controlsTimeoutMouseLeave);
 							}
 						}
 					}
